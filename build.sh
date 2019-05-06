2954a3094e6ac649ee5c71a03702717e2bd1701b
荒野無燈 <i@80x86.io>
2019-05-07 00:43:00 +0800

    fixup build.sh

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-
 docker image prune -f
 docker build -f ./docker/Dockerfile -t 80x86/lychee:AMD64-full-latest .
 docker push 80x86/lychee:AMD64-full-latest
+docker tag 80x86/lychee:AMD64-full-latest registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:AMD64-full-latest
 docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:AMD64-full-latest
 
 docker image rm -f 80x86/lychee:arm64-full-latest
@@ -17,6 +18,7 @@ docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-
 docker image prune -f
 docker build -f ./docker/Dockerfile.arm64 -t 80x86/lychee:arm64-full-latest .
 docker push 80x86/lychee:arm64-full-latest
+docker tag 80x86/lychee:arm64-full-latest registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:arm64-full-latest
 docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:arm64-full-latest
 echo "end build full version ..."
 
