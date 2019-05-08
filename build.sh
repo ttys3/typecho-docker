0d3cceaeee81edee94f0563fd5327e0c60d6817b
荒野無燈 <i@80x86.io>
2019-05-08 19:53:25 +0800

    build script: mod for typecho

--- build.sh
+++ build.sh
@@ -2,24 +2,22 @@
 
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
-git checkout master
-
 echo "begin build full version ..."
-docker image rm -f 80x86/lychee:AMD64-full-latest
-docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:AMD64-full-latest
+docker image rm -f 80x86/typecho:amd64
+docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 docker image prune -f
-docker build -f ./docker/Dockerfile -t 80x86/lychee:AMD64-full-latest .
-docker push 80x86/lychee:AMD64-full-latest
-docker tag 80x86/lychee:AMD64-full-latest registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:AMD64-full-latest
-docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:AMD64-full-latest
+docker build -f ./docker/Dockerfile -t 80x86/typecho:amd64 .
+docker push 80x86/typecho:amd64
+docker tag 80x86/typecho:amd64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
+docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:amd64
 
-docker image rm -f 80x86/lychee:arm64-full-latest
-docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:arm64-full-latest
+docker image rm -f 80x86/typecho:arm64
+docker image rm -f registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
 docker image prune -f
-docker build -f ./docker/Dockerfile.arm64 -t 80x86/lychee:arm64-full-latest .
-docker push 80x86/lychee:arm64-full-latest
-docker tag 80x86/lychee:arm64-full-latest registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:arm64-full-latest
-docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-lychee-alpine-s6:arm64-full-latest
+docker build -f ./docker/Dockerfile.arm64 -t 80x86/typecho:arm64 .
+docker push 80x86/typecho:arm64
+docker tag 80x86/typecho:arm64 registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
+docker push registry.cn-shenzhen.aliyuncs.com/ttys0/docker-typecho-alpine-s6:arm64
 echo "end build full version ..."
 
 ./multi.sh
--- multi.sh
+++ multi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker manifest create 80x86/lychee:latest 80x86/lychee:AMD64-full-latest 80x86/lychee:arm64-full-latest
-docker manifest annotate 80x86/lychee:latest 80x86/lychee:AMD64-full-latest --os linux --arch amd64
-docker manifest annotate 80x86/lychee:latest 80x86/lychee:arm64-full-latest --os linux --arch arm64
-docker manifest push 80x86/lychee:latest --purge
+docker manifest create 80x86/typecho:latest 80x86/typecho:amd64 80x86/typecho:arm64
+docker manifest annotate 80x86/typecho:latest 80x86/typecho:amd64 --os linux --arch amd64
+docker manifest annotate 80x86/typecho:latest 80x86/typecho:arm64 --os linux --arch arm64
+docker manifest push 80x86/typecho:latest --purge
