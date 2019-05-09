1a5fecbed2a9a059426e16daeb4b4219a49700f8
荒野無燈 <i@80x86.io>
2019-05-09 11:19:50 +0800

    fixup init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-rm -rf typecho
-git clone -b nanodm --recurse-submodules https://github.com/ttys3/typecho.git
-
 ROOT_DIR=`pwd`
-cd typecho/usr/themes
-
-git clone https://github.com/Dreamer-Paul/Single.git single
-git clone https://github.com/ttys3/typecho-theme-amaze.git amaze
-git clone https://github.com/shiyiya/typecho-theme-sagiri.git Sagiri
-git clone https://github.com/Siphils/Typecho-Theme-Aria.git Aria
+rm -rf typecho
+git clone -b nanodm --recurse-submodules https://github.com/ttys3/typecho.git && \
+cd typecho/usr/themes && \
+git clone https://github.com/Dreamer-Paul/Single.git single  && \
+git clone https://github.com/ttys3/typecho-theme-amaze.git amaze && \
+git clone https://github.com/shiyiya/typecho-theme-sagiri.git Sagiri && \
+git clone https://github.com/Siphils/Typecho-Theme-Aria.git Aria && \
 git clone https://github.com/Seevil/fantasy.git fantasy
 
-cd $ROOT_DIR/typecho/usr/plugins
-git clone https://github.com/ayangyuan/Youtube-Typecho-Plugin Youtube
-git clone https://github.com/Dreamer-Paul/Pio.git Pio
+cd $ROOT_DIR/typecho/usr/plugins && \
+git clone https://github.com/ayangyuan/Youtube-Typecho-Plugin Youtube && \
+git clone https://github.com/Dreamer-Paul/Pio.git Pio && \
 git clone https://github.com/Copterfly/CodeHighlighter-for-Typecho.git CodeHighlighter
 
 # find . -type d -name Tests -o -name tests -o -name doc -o -name docs | xargs rm -rf
